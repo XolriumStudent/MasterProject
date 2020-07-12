3fa9bb791d48fe7e49476a311bdeb857f27d2aac
Xolrium <ernestohh97@gmail.com>
2020-07-12 22:19:50 +0200

    Tested New Assets

--- Abstract Prototype/Assets/Scripts/Enemies/NPCObjectController.cs
+++ Abstract Prototype/Assets/Scripts/Enemies/NPCObjectController.cs
@@ -6,21 +6,37 @@ public class NPCObjectController : MonoBehaviour
     public EnemyType npcType;
     public NPCScriptableObject npcScriptableObject;
 
-    public void Awake()
+    private void Awake()
     {
         transform.SetParent(GameObject.Find("NPCObjectManager").transform);
     }
 
+    private void Start()
+    {
+        switch (npcType)
+        {
+            case EnemyType.SMALL:
+                AssignVariablesSMALL();
+                break;
+            case EnemyType.NORMAL:
+                AssignVariablesNORMAL();
+                break;
+            case EnemyType.BIG:
+                AssignVariablesBIG();
+                break;
+        }
+    }
+
     public void AssignVariablesSMALL()
     {
-
+        GetComponent<Renderer>().material.color = Color.green;
     }
     public void AssignVariablesNORMAL()
     {
-
+        GetComponent<Renderer>().material.color = Color.red;
     }
     public void AssignVariablesBIG()
     {
-
+        GetComponent<Renderer>().material.color = Color.blue;
     }
 }
